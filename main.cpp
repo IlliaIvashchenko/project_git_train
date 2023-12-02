76585a5b5fd04e58b5301239b265ffc232162f00
Elliyh <elliyha14@gmail.com>
2023-12-02 15:12:46 +0100

    Funkcja "Policz" teraz liczy mnożenie liczb a i b

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b)
+{
+    return a*b;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << " a * b = " << Policz(7, 10) << std::endl;
     return 0;
 }
