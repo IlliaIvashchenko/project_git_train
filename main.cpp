cd00dd76fbe5deb30e14b77a4d47619f0e77c8b1
Elliyh <elliyha14@gmail.com>
2023-12-02 15:08:00 +0100

    Dodałem funkcję do policzenia sumy a i b

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b)
+{
+    return a+b;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << Policz(6, 9) << std::endl;
     return 0;
 }
